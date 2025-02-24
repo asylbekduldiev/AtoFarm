b82b71403b96be836ed12b23289980157e335d61
Asyl <duldievasylbek@gmail.com>
2025-02-24 09:53:19 +0500

    sidebar/subscriber-interface

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 import { NgForOf,NgFor, CommonModule } from '@angular/common';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIconComponent, NgFor, CommonModule],
+  imports: [SvgIconComponent, NgFor, CommonModule, SubscriberCardComponent, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
--- src/app/common-ui/sidebar/subscriber-card/subscriber-card.component.ts
+++ src/app/common-ui/sidebar/subscriber-card/subscriber-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-subscriber-card',
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './subscriber-card.component.html',
   styleUrl: './subscriber-card.component.scss'
 })
 export class SubscriberCardComponent {
-
+  @Input() profile!: Profile;
 }
